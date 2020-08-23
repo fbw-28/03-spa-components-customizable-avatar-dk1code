80e5d80bbdccab4f8a49647aac6fe55088b24a14
dk1code <dk1code@web.de>
2020-08-23 16:02:57 +0200

    finished 03; included mapping of all requested formats

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from "react";
 
+import data from "./data.json";
+
+import "./App.css"
+
 import Avatar from "./Avatar";
 
-function App() {
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Customizable Avatar</h1>
+//       {/* <Avatar size="xl" shape="circle" sizeText="200x200" /> */}
+// <main className="main-container">
+//         {data.map((item) => (
+//           <Avatar size="item.size" shape="item.type" sizeText="item.sides" />
+//         )}
+//         </main>
+//         </div>
+//   );
+// }
+
+export default function App() {
   return (
-    <div className="App">
-      <Avatar size="xl" shape="circle" />
+    <div>
+      <h1>Customizable Avatar</h1>
+
+      <main className="main-container">
+        {data.map((item) => (
+          <Avatar size={item.size} shape={item.type} sizeText={item.sides} />
+        ))}
+      </main>
     </div>
   );
 }
-
-export default App;
--- src/Avatar.js
+++ src/Avatar.js
@@ -2,15 +2,16 @@ import React from "react";
 
 import "./Avatar.css";
 
-export default function Avatar({size, shape}) {
+export default function Avatar({ size, shape, sizeText }) {
   return (
     <div className="card">
       <img
         src="https://picsum.photos/400/400"
         className={`${size} ${shape}`}
+        alt="image"
       ></img>
       <p>
-              size: <span>{`${size}`}</span> with 200x200px
+        size: <span>{`${size}`}</span> with {`${sizeText}`}px
       </p>
       <p>
         type: <span>{`${shape}`}</span>
